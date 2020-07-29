d67a4577cc61c9c259a183f009815017bb7af08b
Dilum Aluthge <dilum@aluthge.com>
2020-07-29 10:11:24 -0400

    Add `Person`, and fix CI (#1)

--- src/OMOPCommonDataModel.jl
+++ src/OMOPCommonDataModel.jl
@@ -1,5 +1,69 @@
 module OMOPCommonDataModel
 
-# Write your package code here.
+import Dates
+
+export OmopType
+
+abstract type OmopType end
+
+# Standardized Vocabularies
+# export Concept
+# export Vocabulary
+# export Domain
+# export ConceptClass
+# export ConceptRelationship
+# export Relationship
+# export ConceptSynonym
+# export ConceptAncestor
+# export SourceToConceptMap
+# export DrugStrength
+include("vocabularies.jl")
+
+# Standardized Metadata
+# export CdmSource
+# export Metadata
+include("metadata.jl")
+
+# Standardized Clinical Data Tables
+export Person
+# export ObservationPeriod
+# export Death
+# export VisitOccurence
+# export VisitDetail
+# export ConditionOccurence
+# export DrugExposure
+# export ProcedureOccurence
+# export DeviceExposure
+# export Measurement
+# export Note
+# export NoteNlp
+# export SurveyConduct
+# export Observation
+# export Specimen
+# export FactRelationship
+include("clinical-data-tables.jl")
+
+# Standardized Health System Data Tables
+# export Location
+# export LocationHistory
+# export CareSite
+# export Provider
+include("health-system-data-tables.jl")
+
+# Standardized Health Economics Data Tables
+# export PayerPlanPeriod
+# export Cost
+include("health-economics-data-tables.jl")
+
+# Standardized Derived Elements
+# export DrugEra
+# export DoseEra
+# export ConditionEra
+include("derived-elements.jl")
+
+# Results Schema
+# export Cohort
+# export CohortDefinition
+include("results-schema.jl")
 
 end
