b7c4726560460524f2116a43884af74983616743
Dilum Aluthge <dilum@aluthge.com>
2020-08-02 04:03:22 +0000

    Reorganize the source files

---
+++ src/utils.jl
@@ -0,0 +1,19 @@
+import Pkg
+
+"""
+The version of the `OMOPCommonDataModel.jl` Julia package.
+"""
+function version()::VersionNumber
+    package_directory = dirname(dirname(@__FILE__))
+    project_file = joinpath(package_directory, "Project.toml")
+    version_string = Pkg.TOML.parsefile(project_file)["version"]
+    version_number = VersionNumber(version_string)
+    return version_number
+end
+
+"""
+The version of the OMOP Common Data Model (CDM) being implemented.
+"""
+@inline function cdm_version()::VersionNumber
+    return CDM_VERSION
+end
